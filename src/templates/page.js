f0d5921ecf5d6a928d301e16f2dfccd171402430
daniel <itzdanielmichael@gmail.com>
2023-05-16 16:56:58 -0400

    add comment

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
 
 function Page(props) {
+  // pageContext is passed from gatsby-node.js in the createPage function (context property)
   return <BlockRendererProvider allBlocks={props.pageContext.blocks} />;
 }
 
