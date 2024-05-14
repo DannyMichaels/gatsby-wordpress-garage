8fc1284e077538c5223eb2d42ef4a4cc235c6005
daniel <itzdanielmichael@gmail.com>
2024-05-14 15:48:56 -0400

    add menu links

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 const mainMenuQuery = graphql`
   query MainMenuQuery {
@@ -41,10 +41,40 @@ const mainMenuQuery = graphql`
 export const Menu = () => {
   const data = useStaticQuery(mainMenuQuery);
 
-  console.log("MAIN MENU DATA", data);
+  const { menuItems = [] } = data.wp.acfOptionsMainMenu.mainMenu;
+
   return (
-    <div className="sticky top-0 z-20 h-16 bg-emerald-900 px-4 font-bold text-white">
-      Menu
+    <div
+      className="sticky 
+    top-0 z-20
+    flex
+    h-16 
+    items-center
+    justify-between bg-emerald-900 px-4 font-bold text-white"
+    >
+      <div>logo</div>
+      <div className="flex h-full space-x-4">
+        {menuItems.map((menuItem, idx) => (
+          <div
+            key={idx}
+            className="flex h-full cursor-pointer hover:bg-emerald-800"
+          >
+            <Link
+              to={menuItem.root.destination.uri}
+              className="
+              flex
+              h-full
+              items-center
+              px-4
+              text-white
+              no-underline
+            "
+            >
+              {menuItem.root.label}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
